2c8bec72d4a0ca3d751bdddf3da7299f435ec804
Ubuntu <ubuntu@ip-172-31-17-190.ap-south-1.compute.internal>
2021-06-28 17:32:07 +0000

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0)
+  const increaseByTwo = () => {
+    for (let i=0; i< 2; i++) {
+      setCount(prevCount => prevCount + 1)
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+          Learn React Hooks
       </header>
+      <h4>Value of count: {count}</h4>
+      <button onClick={() => setCount(count + 1)}>Increase Count</button>
+      <button onClick={() => setCount(count - 1)}>Decrease Count</button>
+      <button onClick={increaseByTwo}>Increase By two</button>
+      <ArrayApp/>
     </div>
   );
+}
+
+const ArrayApp = () => {
+  const [array, setArray ] = useState([]);
+  const setNumber = () => {
+    setArray(
+      [
+        ...array,
+        {
+          id: array.length,
+          value: Math.round(Math.random() * 1000)
+        }
+      ]
+    )
+  }
+
+  return (
+    <>
+      <button onClick={setNumber}>Add Number</button>
+      {
+        array.map((item, index) => {
+          return <div key={item.id}>{item.value}</div>
+        })
+      }
+    </>
+  )
 }
 
 export default App;
